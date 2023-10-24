62522f8660df8a82261e895b4aa1fb04fec5e45a
iguimara <igor.guimaraes@t-systems.com>
2023-10-24 12:20:31 -0300

    creating endpoints to access vms infos

--- README.md
+++ README.md
@@ -1,1 +1,343 @@
-# vmware-rest-api
+# VMWare Rest API PHP
+
+Copyright 2023 Igor Guimarães
+
+Version 1.0
+
+## Description
+
+Basic class PHP compiling the main vCenter API calls, making it possible to simultaneously manage more than one virtualization environment.
+
+## Important
+
+* For more information, visit the official API link: https://developer.vmware.com/apis/vsphere-automation/latest/vcenter/
+* The methods present in the class were added to the vCenter framework after version 7.0 U2
+
+## Authentication
+
+The vCenter API supports the following methods to authenticate requests. Individual operations in the documentation will include their specific authentication types.
+
+* api_key
+  * Type: APIKey
+  * In: Header
+  * Name: vmware-api-session-id
+* basic_auth
+  * Type: HTTP
+  * In: Header
+  * Scheme: Basic
+
+**Attention Point!!** the API key is only valid for 5 minutes and by default and vCenter allows the simultaneous creation of a maximum of 50 keys per environment, of which it is advisable that after finishing using the key, destroy it of the same before creating a new one.
+
+#### Basic Auth Ex ...
+
+```
+<?php
+include_once  "..\src\vmwareRestPHP.php";
+
+$ipaddress = '160.190.0.35';
+$username = 'teste';
+$password = 'teste1234';
+
+$api = new vmwareRestApi($ipaddress, $username, $password);
+$session_id = $api->vmwareCreateSessionId();
+
+print_r($session_id);
+
+```
+
+return:
+
+```
+{
+    "status" => true,
+    "data" => {
+        "value" => "9a868d715ad5411a39b75f5fef482e9b"
+    }
+}
+```
+
+#### Destroy Session
+
+```
+$api->vmwareDeleteSessionId(api->vmwareDeleteSessionId($session_id);
+```
+
+### Access with NGINX
+
+If it is necessary to access an environment through a reverse proxy using NGINX, the class allows the reverse proxy access configuration to be carried out.
+
+```
+$reverse_proxy = '1.10.0.3:9008';
+$nginx_username = 'teste_nginx';
+$nginx_password = 'mginx_teste1234';
+$username = 'teste';
+$password = 'teste1234';
+
+$api = new vmwareRestApi($reverse_proxy, $nginx_username, $nginx_password);
+$session_id = $api->vmwareCreateSessionId(["vmware-auth: Basic " . base64_encode("{$username}:{$password}")]);
+```
+
+**Attention Point!!** The 'vmware-auth' variable must be configured with the same name within NGINX so that when it directs to the correct IP, this new auth is assigned. Otherwise, the reverse proxy Auth will be sent, which for security reasons must be different from that used by the VMWare environment.
+
+## Virtual Machines
+
+The vcenter vm package provides services for managing virtual machines.
+
+* VM List
+* VM Details
+* Create VM
+* Delete VM
+* VM Tools
+* VM Guest
+* VM Hardware Basic
+* VM HArdware Adapter
+* VM Hardware Boot
+* VM Hardware Info
+* VM Power
+* VM Storage Policy
+
+### VM List & Details
+
+Returns information about at most 4000 visible (subject to permission checks) virtual machines in vCenter matching the VM.FilterSpec.
+
+```
+//List All vms
+$vms = $api->getVirtualMachines($session_id['data']['value']);
+
+/* return info
+{
+    "status" => true,
+    "data" => {
+        "value" => {
+            {
+                "memory_size_MiB": 51200,
+                "vm": "vm-105875",
+                "name": "Teste_vm1",
+                "power_state": "POWERED_ON",
+                "cpu_count": 9
+            },
+            {
+                "memory_size_MiB": 221184,
+                "vm": "vm-108158",
+                "name": "Teste_vm2",
+                "power_state": "POWERED_OFF",
+                "cpu_count": 20
+            },
+        }
+    }
+}
+*/
+
+
+//VM detail
+$vm = $api->getVirtualMachines($session_id['data']['value'], [], $vms['data']['value'][0]['vm']);
+
+/* return info
+{
+    "status" => true,
+    "data" => {
+    "value": {
+        "instant_clone_frozen": false,
+        "cdroms": [
+            {
+                "value": {
+                    "start_connected": false,
+                    "backing": {
+                        "device_access_type": "EMULATION",
+                        "type": "CLIENT_DEVICE"
+                    },
+                    "allow_guest_control": true,
+                    "label": "CD/DVD drive 1",
+                    "state": "NOT_CONNECTED",
+                    "type": "SATA",
+                    "sata": {
+                        "bus": 0,
+                        "unit": 0
+                    }
+                },
+                "key": "16000"
+            }
+        ],
+        "memory": {
+            "hot_add_increment_size_MiB": 0,
+            "size_MiB": 51200,
+            "hot_add_enabled": false,
+            "hot_add_limit_MiB": 51200
+        },
+        "disks": [
+            {
+                "value": {
+                    "scsi": {
+                        "bus": 0,
+                        "unit": 0
+                    },
+                    "backing": {
+                        "vmdk_file": "[Teste] Teste_vm1/Teste_vm1.vmdk",
+                        "type": "VMDK_FILE"
+                    },
+                    "label": "Hard disk 1",
+                    "type": "SCSI",
+                    "capacity": 107374182400
+                },
+                "key": "2000"
+            },
+            {
+                "value": {
+                    "scsi": {
+                        "bus": 0,
+                        "unit": 1
+                    },
+                    "backing": {
+                        "vmdk_file": "[Teste] Teste_vm1/Teste_vm1_10.vmdk",
+                        "type": "VMDK_FILE"
+                    },
+                    "label": "Hard disk 2",
+                    "type": "SCSI",
+                    "capacity": 214748364800
+                },
+                "key": "2001"
+            }
+        ],
+        "parallel_ports": [],
+        "sata_adapters": [
+            {
+                "value": {
+                    "bus": 0,
+                    "label": "SATA controller 0",
+                    "type": "AHCI"
+                },
+                "key": "15000"
+            }
+        ],
+        "cpu": {
+            "hot_remove_enabled": false,
+            "count": 9,
+            "hot_add_enabled": false,
+            "cores_per_socket": 3
+        },
+        "scsi_adapters": [
+            {
+                "value": {
+                    "scsi": {
+                        "bus": 0,
+                        "unit": 7
+                    },
+                    "label": "SCSI controller 0",
+                    "sharing": "NONE",
+                    "type": "PVSCSI"
+                },
+                "key": "1000"
+            },
+            {
+                "value": {
+                    "pci_slot_number": 1184,
+                    "scsi": {
+                        "bus": 1,
+                        "unit": 7
+                    },
+                    "label": "SCSI controller 1",
+                    "sharing": "NONE",
+                    "type": "PVSCSI"
+                },
+                "key": "1001"
+            }
+        ],
+        "power_state": "POWERED_ON",
+        "floppies": [],
+        "identity": {
+            "name": "Teste_vm1",
+            "instance_uuid": "5028198f-5f09-7c36-5956-16e1ac99641c",
+            "bios_uuid": "4228c71f-4d7d-967f-0831-b4180e5b207c"
+        },
+        "nvme_adapters": [],
+        "name": "ap3std0518",
+        "nics": [
+            {
+                "value": {
+                    "start_connected": true,
+                    "pci_slot_number": 192,
+                    "backing": {
+                        "connection_cookie": 74116667,
+                        "distributed_port": "3542",
+                        "distributed_switch_uuid": "50 28 b4 87 38 cf 82 26-b8 56 4e 43 53 65 a6 9c",
+                        "type": "DISTRIBUTED_PORTGROUP",
+                        "network": "dvportgroup-10482"
+                    },
+                    "mac_address": "00:50:56:a8:85:2a",
+                    "mac_type": "ASSIGNED",
+                    "allow_guest_control": true,
+                    "wake_on_lan_enabled": true,
+                    "label": "Network adapter 1",
+                    "state": "CONNECTED",
+                    "type": "VMXNET3",
+                    "upt_compatibility_enabled": true
+                },
+                "key": "4000"
+            },
+            {
+                "value": {
+                    "start_connected": true,
+                    "pci_slot_number": 224,
+                    "backing": {
+                        "connection_cookie": 74123494,
+                        "distributed_port": "2438",
+                        "distributed_switch_uuid": "50 28 b4 87 38 cf 82 26-b8 56 4e 43 53 65 a6 9c",
+                        "type": "DISTRIBUTED_PORTGROUP",
+                        "network": "dvportgroup-10483"
+                    },
+                    "mac_address": "00:50:56:a8:42:20",
+                    "mac_type": "ASSIGNED",
+                    "allow_guest_control": true,
+                    "wake_on_lan_enabled": true,
+                    "label": "Network adapter 2",
+                    "state": "CONNECTED",
+                    "type": "VMXNET3",
+                    "upt_compatibility_enabled": true
+                },
+                "key": "4001"
+            },
+            {
+                "value": {
+                    "start_connected": true,
+                    "pci_slot_number": 256,
+                    "backing": {
+                        "connection_cookie": 74128857,
+                        "distributed_port": "232",
+                        "distributed_switch_uuid": "50 28 b4 87 38 cf 82 26-b8 56 4e 43 53 65 a6 9c",
+                        "type": "DISTRIBUTED_PORTGROUP",
+                        "network": "dvportgroup-65"
+                    },
+                    "mac_address": "00:50:56:a8:1b:47",
+                    "mac_type": "ASSIGNED",
+                    "allow_guest_control": true,
+                    "wake_on_lan_enabled": true,
+                    "label": "Network adapter 3",
+                    "state": "CONNECTED",
+                    "type": "VMXNET3",
+                    "upt_compatibility_enabled": true
+                },
+                "key": "4002"
+            }
+        ],
+        "boot": {
+            "delay": 5000,
+            "retry_delay": 10000,
+            "enter_setup_mode": false,
+            "type": "BIOS",
+            "retry": false
+        },
+        "serial_ports": [],
+        "boot_devices": [],
+        "guest_OS": "SLES_11_64",
+        "hardware": {
+            "upgrade_policy": "NEVER",
+            "upgrade_status": "NONE",
+            "version": "VMX_13"
+        }
+    }
+}
+}
+*/
+
+
+```
---
+++ src/vmwareRestPHP.php
@@ -0,0 +1,421 @@
+<?php
+
+// VMWareAPI version 1.0
+// Copyright 2023 Igor Guimarães
+
+namespace vmwareRestPHP;
+
+class vmwareRestPHP
+{
+
+    /**
+     * The URL to the VMWare Rest API.
+     * Ex. "https://190.160.0.5/rest"
+     * @var string
+     */
+    private $base_url;
+
+    /**
+     * The Hash to allow access to the API.
+     * Using username and password to generate hash in base64_encode().
+     * @var string
+     */
+    private $hash;
+
+    /**
+     * Setting security in cURL to the access API
+     * @var bool
+     */
+    private $secure;
+
+    /**
+     * Setting proxy to the access virtualization environment
+     * @var string
+     */
+    private $proxy;
+
+    /**
+     * Set headers
+     * @var array
+     */
+    private $headers = [];
+
+    /**
+     * Standard settings headers need to access Rest API
+     * @var array
+     */
+    private $headers_std = [
+        'Accept: */*',
+        'Accept-Encoding: gzip, deflate, br',
+        'Connection: keep-alive',
+        'User-Agent: curl/7.54.0'
+    ];
+
+    /**
+     * Basic settings to access infos in the vcenter
+     * @param String $ipaddress The ipaddress of the vcenter.
+     * @param String $username The username with allow access vcenter
+     * @param String $password The password with allow access vcenter
+     * @param bool $secure Validate this used security flag
+     * @param String $proxy A proxy to access the virtual environment with jump-server
+     */
+    public function __construct(String $ipaddress, String $username, String $password, bool $secure = false, String $proxy = '') {
+
+        $this->base_url = "https://{$ipaddress}/rest";
+        $this->hash = base64_encode("$username:$password");
+        $this->secure = $secure;
+        $this->proxy = $proxy;
+
+    }
+
+    /**********************
+     * Internal functions *
+     **********************/
+
+    /**
+     * Set headers to access API
+     * @param String|array|null $opts Set extra opsets to access endpoints
+     * @param bool $set_auth Setting basic auth. Using to create session ID.
+     * @return String
+     */
+    private function __setHeaders($opts = null, bool $set_auth = false) : String
+    {
+        if($set_auth) {
+            $this->headers = $this->headers_std;
+            $this->headers[] = "Authorization: Basic {$this->hash}";
+        }
+
+        if(!empty($opts) && is_array($opts)) {
+            foreach ($opts as $opt) {
+                $this->headers[] = $opt;
+            }
+        } else {
+            $this->headers[] = $opts;
+        }
+
+        return "Updated headers with auth and opt info ...";
+    }
+
+    /**
+     * Function responsible for calling endpoints and processing the return
+     * @param String $endpoint_uri Set endpoint in API to the access is required
+     * @param String $request_type Define the call request type
+     * @param array $parameters Set others parameters wanted to the correct return API
+     * @return array
+     */
+    private function __sendRequest(String $endpoint_uri, String $request_type = 'GET', array $parameters = []) : array
+    {
+
+        try {
+
+            $ch = curl_init();
+            $url = $this->base_url . $endpoint_uri;
+            curl_setopt($ch, CURLOPT_POST, $request_type !== 'GET');
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_PROXY, $this->proxy);
+            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $request_type);
+            curl_setopt($ch, CURLOPT_ENCODING, "");
+            curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
+            curl_setopt($ch, CURLOPT_FAILONERROR,false);
+            curl_setopt($ch, CURLOPT_VERBOSE, false);
+            curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 120);
+            curl_setopt($ch, CURLOPT_TIMEOUT, 60); //timeout in seconds
+            curl_setopt($ch, CURLOPT_HTTPHEADER, $this->headers); //timeout in seconds
+
+            if(!$this->secure){
+                curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+                curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+            }
+
+            if(!empty($parameters)){
+                if($request_type == 'GET') {
+                    $url .= sprintf("?%s", http_build_query($parameters));
+                } else {
+                    $payload = json_encode($parameters, JSON_NUMERIC_CHECK);
+                    $this->headers[] = 'Content-Type: application/json';
+
+                    if($request_type !== 'PUT') $this->headers[] = 'Content-Length: ' . strlen($payload);
+
+                    curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
+                }
+            }
+
+            curl_setopt($ch, CURLOPT_URL, $url);
+            $response = json_decode(curl_exec($ch),true);
+            curl_close($ch);
+
+            if(isset($response['type']) && $response['type'] === 'com.vmware.vapi.std.errors.unauthenticated') return ['status' => false,  'data' => $response];
+            else return ['status' => true,  'data' => $response];
+
+        } catch(\Exception $e) {
+            return ["status" => false, "code" => $e->getCode(), "data" => $e->getMessage(), "trace" => $e->getTraceAsString(), "timestamp" => date("Y-m-d H:i:s")];
+        }  catch (\Throwable $e) {
+            return ["status" => false, "code" => $e->getCode(), "data" => $e->getMessage(), "trace" => $e->getTraceAsString(), "timestamp" => date("Y-m-d H:i:s")];
+        }
+
+    }
+
+
+    /*********************
+     * Session Functions *
+     *********************/
+    /**
+     * Creates the session ID that allows access to the environments.
+     * It is mandatory to use it in version 7.4.u or higher of vsphere for the API to validate access.
+     * ID is valid to 5 minutes.
+     * @param array $headers Set other headers infos
+     * @return array
+     */
+    public function vmwareCreateSessionId(array $headers = []) :array {
+        try{
+            $this->__setHeaders($headers, true);
+            return $this->__sendRequest("/com/vmware/cis/session",'POST');
+        }catch(\Exception $e){
+            return ['status' => false, 'data' => $e->getMessage()];
+        }catch(\Throwable $err){
+            return ['status' => false, 'data' => $err->getMessage()];
+        }
+    }
+
+    /**
+     * Removes the created session id from the list of valid ids for access.
+     * For security, vsphere only allows you to create 50 concurrent sessions per instance by default.
+     * @param String $sessionId Previously created session id
+     * @return array
+     */
+    public function vmwareDeleteSessionId(String $sessionId) :array {
+        try{
+            $this->__setHeaders("vmware-api-session-id: $sessionId");
+            return $this->__sendRequest("/com/vmware/cis/session",'DELETE');
+        }catch(\Exception $e){
+            return ['status' => false, 'data' => $e->getMessage()];
+        }catch(\Throwable $err){
+            return ['status' => false, 'data' => $err->getMessage()];
+        }
+    }
+
+
+    /****************************************
+     * Public functions to virtual machines *
+     ****************************************/
+    /**
+     * List the basic information of the VMs that are in vcenter.
+     * @param String $sessionId Previously created session id
+     * @param array $params
+     * @param String|null $vmId
+     * @return array
+     */
+    public function getVirtualMachines(String $sessionId, array $params = [], String $vmId = null) : array {
+        try{
+            $this->__setHeaders("vmware-api-session-id: $sessionId");
+            $endpoint = is_null($vmId) ? "/vcenter/vm" : "/vcenter/vm/$vmId";
+            return $this->__sendRequest($endpoint,'GET', $params);
+        }catch(\Exception $e){
+            return ['status' => false, 'data' => $e->getMessage()];
+        }catch(\Throwable $err){
+            return ['status' => false, 'data' => $err->getMessage()];
+        }
+    }
+
+    /**
+     * @param String $sessionId Previously created session id
+     * @param array $params
+     * @return array
+     */
+    public function setNewVirtualMachine(String $sessionId, array $params = []) : array {
+        try{
+            $this->__setHeaders("vmware-api-session-id: $sessionId");
+            return $this->__sendRequest("/vcenter/vm",'POST', $params);
+        }catch(\Exception $e){
+            return ['status' => false, 'data' => $e->getMessage()];
+        }catch(\Throwable $err){
+            return ['status' => false, 'data' => $err->getMessage()];
+        }
+    }
+
+    /**
+     * @param String $sessionId Previously created session id
+     * @param String $vmId
+     * @return array
+     */
+    public function deleteVirtualMachine(String $sessionId, String $vmId) : array {
+        try{
+            $this->__setHeaders("vmware-api-session-id: $sessionId");
+            return $this->__sendRequest("/vcenter/vm/$vmId",'DELETE');
+        }catch(\Exception $e){
+            return ['status' => false, 'data' => $e->getMessage()];
+        }catch(\Throwable $err){
+            return ['status' => false, 'data' => $err->getMessage()];
+        }
+    }
+
+    /**
+     * @param String $sessionId Previously created session id
+     * @param String $vmId
+     * @return array
+     */
+    public function getVmTools(String $sessionId, String $vmId) : array{
+        try {
+            $this->__setHeaders("vmware-api-session-id: $sessionId");
+            return $this->__sendRequest("/vcenter/vm/$vmId/tools",'GET');
+        }catch(\Exception $e){
+            return ['status' => false, 'data' => $e->getMessage()];
+        }catch(\Throwable $err){
+            return ['status' => false, 'data' => $err->getMessage()];
+        }
+    }
+
+    public function getVmGuest(String $sessionId, String $vmId, String $info = null) : array {
+        try{
+
+            $validate = ['identity', 'local-filesystem', 'power'];
+
+            if(in_array($info, $validate)) {
+                $this->__setHeaders("vmware-api-session-id: $sessionId");
+                return $this->__sendRequest("/vcenter/vm/$vmId/guest/$info",'GET');
+            } else {
+                return ['status' => false, 'data' => "Invalid request. Valid Options: " . json_encode($validate)];
+            }
+        }catch(\Exception $e){
+            return ['status' => false, 'data' => $e->getMessage()];
+        }catch(\Throwable $err){
+            return ['status' => false, 'data' => $err->getMessage()];
+        }
+    }
+
+    public function getVmHardwareBasic(String $sessionId, String $vmId) : array {
+        try{
+            $this->__setHeaders("vmware-api-session-id: $sessionId");
+            return $this->__sendRequest("/vcenter/vm/$vmId/hardware",'GET');
+        }catch(\Exception $e){
+            return ['status' => false, 'data' => $e->getMessage()];
+        }catch(\Error $err){
+            return ['status' => false, 'data' => $err->getMessage()];
+        }
+    }
+
+    public function getVmHardwareAdapter(String $sessionId, String $vmId, String $type = null, String $adapterId = null) : array {
+        try{
+            $validate = ['sata', 'scsi'];
+
+            if(in_array($type, $validate)) {
+                $this->__setHeaders("vmware-api-session-id: $sessionId");
+                $endpoint = is_null($adapterId) ? "/vcenter/vm/$vmId/hardware/$type" : "/vcenter/vm/$vmId/hardware/$type/$adapterId";
+                return $this->__sendRequest($endpoint,'GET');
+            } else {
+                return ['status' => false, 'data' => "Invalid request. Valid Options: " . json_encode($validate)];
+            }
+        }catch(\Exception $e){
+            return ['status' => false, 'data' => $e->getMessage()];
+        }catch(\Error $err){
+            return ['status' => false, 'data' => $err->getMessage()];
+        }
+    }
+
+    public function getVmHardwareBoot(String $sessionId, String $vmId, bool $device = false) : array {
+        try{
+            $this->__setHeaders("vmware-api-session-id: $sessionId");
+            $endpoint = $device ? "/vcenter/vm/$vmId/hardware/boot" : "/vcenter/vm/$vmId/hardware/boot/device";
+            return $this->__sendRequest($endpoint,'GET');
+        }catch(\Exception $e){
+            return ['status' => false, 'data' => $e->getMessage()];
+        }catch(\Error $err){
+            return ['status' => false, 'data' => $err->getMessage()];
+        }
+    }
+
+    public function getVmHardwareInfo(String $sessionId, String $vmId, String $type = null, String $Id = null) : array {
+        try{
+            $validate = ['cdrom', 'cpu', 'disk', 'ethernet', 'floppy', 'memory', 'parallel', 'serial'];
+
+            if(in_array($type, $validate)) {
+                $this->__setHeaders("vmware-api-session-id: $sessionId");
+                $endpoint = is_null($Id) ? "/vcenter/vm/$vmId/hardware/$type" : "/vcenter/vm/$vmId/hardware/$type/$Id";
+                return $this->__sendRequest($endpoint,'GET');
+            } else {
+                return ['status' => false, 'data' => "Invalid request. Valid Options: " . json_encode($validate)];
+            }
+        }catch(\Exception $e){
+            return ['status' => false, 'data' => $e->getMessage()];
+        }catch(\Error $err){
+            return ['status' => false, 'data' => $err->getMessage()];
+        }
+    }
+
+    public function getVmPower(String $sessionId, String $vmId) : array {
+        try{
+            $this->__setHeaders("vmware-api-session-id: $sessionId");
+            $endpoint = "/vcenter/vm/$vmId/power";
+            return $this->__sendRequest($endpoint,'GET');
+        }catch(\Exception $e){
+            return ['status' => false, 'data' => $e->getMessage()];
+        }catch(\Error $err){
+            return ['status' => false, 'data' => $err->getMessage()];
+        }
+    }
+
+    public function getVmStoragePolicy(String $sessionId, String $vmId, bool $compliance = false) : array {
+        try{
+            $this->__setHeaders("vmware-api-session-id: $sessionId");
+            $endpoint = $compliance ? "/vcenter/vm/$vmId/storage/policy/compliance" : "/vcenter/vm/$vmId/storage/policy";
+            return $this->__sendRequest($endpoint,'GET');
+        }catch(\Exception $e){
+            return ['status' => false, 'data' => $e->getMessage()];
+        }catch(\Error $err){
+            return ['status' => false, 'data' => $err->getMessage()];
+        }
+    }
+
+
+    /**
+     * @param String $sessionId Previously created session id
+     * @param array $params
+     * @return array
+     */
+    public function getFolders(String $sessionId, array $params = []) : array {
+        try{
+            $this->__setHeaders("vmware-api-session-id: $sessionId");
+            return $this->__sendRequest("/vcenter/folder",'GET', $params);
+        }catch(\Exception $e){
+            return ['status' => false, 'data' => $e->getMessage()];
+        }catch(\Throwable $err){
+            return ['status' => false, 'data' => $err->getMessage()];
+        }
+    }
+
+    public function getClusters(String $sessionId, String $clusterId = null) : array {
+        try{
+            $this->__setHeaders("vmware-api-session-id: $sessionId");
+            $endpoint = is_null($clusterId) ? "/vcenter/cluster" : "/vcenter/cluster/$clusterId";
+            return $this->__sendRequest($endpoint,'GET');
+        }catch(\Exception $e){
+            return ['status' => false, 'data' => $e->getMessage()];
+        }catch(\Error $err){
+            return ['status' => false, 'data' => $err->getMessage()];
+        }
+    }
+
+    public function getDatacenters(String $sessionId, String $datacenterId = null) : array {
+        try{
+            $this->__setHeaders("vmware-api-session-id: $sessionId");
+            $endpoint = is_null($datacenterId) ? "/vcenter/datacenter" : "/vcenter/datacenter/$datacenterId";
+            return $this->__sendRequest($endpoint,'GET');
+        }catch(\Exception $e){
+            return ['status' => false, 'data' => $e->getMessage()];
+        }catch(\Error $err){
+            return ['status' => false, 'data' => $err->getMessage()];
+        }
+    }
+
+    public function getDatastores(String $sessionId, String $datastoreId = null) : array {
+        try{
+            $this->__setHeaders("vmware-api-session-id: $sessionId");
+            $endpoint = is_null($datastoreId) ? "/vcenter/datastore" : "/vcenter/datastore/$datastoreId";
+            return $this->__sendRequest($endpoint,'GET');
+        }catch(\Exception $e){
+            return ['status' => false, 'data' => $e->getMessage()];
+        }catch(\Error $err){
+            return ['status' => false, 'data' => $err->getMessage()];
+        }
+    }
+}
